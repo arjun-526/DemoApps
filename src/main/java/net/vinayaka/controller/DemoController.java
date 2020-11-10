8eb9b349a48b0524c764b3542e589714db9d66f6
DESKTOP-C4VSTJB\hp <Anke.Nagarjuna@owitglobal.com>
2020-11-10 14:32:39 +0530

    add a name to the controller

--- src/main/java/net/vinayaka/controller/DemoController.java
+++ src/main/java/net/vinayaka/controller/DemoController.java
@@ -1,6 +1,7 @@
 package net.vinayaka.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class DemoController {
 
-    @GetMapping(value = "/hello")
-    public String helloApp(){
-        return "Welcome to Demo Controller";
+    @GetMapping(value = "/hello/{name}")
+    public String helloApp(@PathVariable("name") String name) {
+        return name + "Welcome to Demo Controller";
     }
 }
